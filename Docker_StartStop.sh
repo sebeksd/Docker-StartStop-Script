c87db61b9a02cd15c323d9b143790cc117f8a1a7
sebeksd <sebeksd@interia.pl>
2024-08-06 08:26:29 +0200

    New flag allowing to pass the return value (pass pre script value to post script) Typo fixes

--- Docker_StartStop.sh
+++ Docker_StartStop.sh
@@ -7,25 +7,28 @@
 # Used naming scheme:
 # cNAME - const, vName - variable, vNAME - all caps variable for "Text" (multiline) 
 
+vPreScriptResult=-1
+
 showHelp()
 {
   echo "
 Script to stop running containers and then start them without starting containers that were already stopped.
   
 Additional options are:
-  -s, --stop - will stop running containers and save them to a list
-  -r, --restart - will start containers previously stopped by this script
-  -h, --help - show this help
+  -s - will stop running containers and save them to a list
+  -r - will start containers previously stopped by this script
+  -x - pass the parameter as result, usefull to pass result of command executed before this script
+      Usage example: Docker_StartStop.sh -x \$?
+  -h - show this help
 "
-  exit 1
 }
 
 function restart()
 {
-  echo "Restarting all preciously stopped containers"
+  echo "Restarting all previously stopped containers"
 
-  docker start $(cat /tmp/Docker_StartStop-running_list.tmp) &&\
-    rm /tmp/Docker_StartStop-running_list.tmp
+  docker start $(cat /tmp/Docker_StartStop-runnig_list.tmp) &&\
+    rm /tmp/Docker_StartStop-runnig_list.tmp
 
   echo "All containers restarted"
 }
@@ -38,21 +41,26 @@ function stop()
   # in case someone used stop more than once
   if [ "$(docker ps -aq -f status=running -f status=restarting)" ]; then
     # save list
-    docker stop $(docker ps -q) > /tmp/Docker_StartStop-running_list.tmp
+    docker stop $(docker ps -q) > /tmp/Docker_StartStop-runnig_list.tmp
     echo "All containers stopped"
   else
     echo "No running containers, skipping"
   fi  
 }
 
-for arg in "$@" 
+while getopts rshx: flag
 do
-  case "$arg" in
-    -r | --restart) restart;; # restart previously stopped containers
-    -s | --stop) stop;; # stop running containers and put them on the list
-    -h | --help)
-      showHelp  
-      exit 0
-      ;;
+  case "${flag}" in
+      r) restart;; # restart previously stopped containers
+      s) stop;; # stop running containers and put them on the list
+      x) 
+        # pass the parameter as result, usefull to pass result of command executed before this script
+        vPreScriptResult=${OPTARG};; 
+      h | *) 
+        showHelp;;
   esac
-done;
+done
+
+if [ "$vPreScriptResult" != -1 ]; then
+  exit $vPreScriptResult
+fi  
